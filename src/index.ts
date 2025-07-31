b84199c8c76f6855e06d3cc63439e7dfb6b7d0ff
ayden94 <144667387+ayden94@users.noreply.github.com>
2025-07-31 15:27:07 +0900

    feat: clsx에서 ClassValue 타입 추가

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 
+export type { ClassValue } from 'clsx';
 export { neato } from './neato';
 export type { CompoundVariant, VariantProps, VariantsConfig } from './types';
 export { neatoVariants } from './variants';
